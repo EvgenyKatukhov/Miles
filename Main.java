4104fbadd4c59be39a1ae224a015fbcaaa38d33f
Evgeny Katukhov <e.katuhov@gmail.com>
2022-12-25 17:26:49 +0300

    git commit -m 'add Mile'

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        //Стоимость билета/
+        int cost = 1 000;
+        //Количество рублей для одной бонусной мили/
+        int bonus = 20;
+        System.out println (cost / bonus);
+
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 20000; // стоимость билета
+
+        int miles = 20; // стоимость одной мили
+
+        int bonus = price / miles; // бонусные мили
+        System.out.println("Бонусные мили:  " + bonus);
+    }
+}
